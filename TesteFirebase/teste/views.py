6124102a1900b947ff4af3f91d470f96c11289f8
gelsonrei <gelsonrei@gmail.com>
2020-03-29 11:08:26 -0300

    TesteFirebase primeiro commit

---
+++ TesteFirebase/teste/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+#cred = credentials.Certificate("/home/gcreinal/Documentos/Devel/Django/TesteFirebase/TesteFirebase/teste/testepython-d9243-firebase-adminsdk-yhrg6-8bfa0ca2c7.json")
+cred = credentials.Certificate("TesteFirebase/teste/testepython-d9243-firebase-adminsdk-yhrg6-8bfa0ca2c7.json")
+
+default_app = firebase_admin.initialize_app(cred, {
+    'projectId': 'testepython-d9243',
+})
+
+
+
+def home(request):
+    
+    #export GOOGLE_APPLICATION_CREDENTIALS="/home/gcreinal/Documentos/Devel/Django/TesteFirebase/TesteFirebase/teste/testepython-d9243-firebase-adminsdk-yhrg6-8bfa0ca2c7.json"
+    #cred = credentials.ApplicationDefault()
+  
+    db = firestore.client()
+    doc_ref = db.collection(u'users').document(u'alovelace')
+    doc_ref.set({
+        u'first': u'Ada',
+        u'last': u'Lovelace',
+        u'born': 1815
+    })
+
+    doc_ref = db.collection(u'users').document(u'aturing')
+    doc_ref.set({
+        u'first': u'Alan',
+        u'middle': u'Mathison',
+        u'last': u'Turing',
+        u'born': 1912
+    })
+
+    users_ref = db.collection(u'users')
+    docs = users_ref.stream()
+
+    lista = []
+    for doc in docs:
+        lista.append(doc.to_dict())  
+        print(u'{} => {}'.format(doc.id, doc.to_dict()))
+
+    
+
+    #print(default_app, cred)
+    context = {
+        'docs': lista
+
+    }
+
+    return render(request, 'home.html', context)
---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/gcreinal/Documentos/Devel/Django/TesteFirebase/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
