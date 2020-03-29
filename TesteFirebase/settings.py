3f2129208aaf0efe496acdd92e98144af01563a3
gelsonrei <gelsornei@gmail.com>
2020-03-29 18:27:23 -0300

    Add firebase module depends

--- TesteFirebase/settings.py
+++ TesteFirebase/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%w@xf*3gwsb^7g573&iywc--)m9+#woja5pl0q6!7-1rn9f@3g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
